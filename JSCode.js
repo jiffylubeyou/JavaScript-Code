e484ea513f843ac544ccde32277743a08b68906d
Kenny Strong <kennyspaz@hotmail.com>
2020-02-14 14:09:35 -0700

    1.2

--- JSCode.js
+++ JSCode.js
@@ -2,4 +2,6 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrays.reduce((bigArray, tempArray) =>  { return bigArray.concat(tempArray);}));
 let array = [1, 2, 3, 4];
 console.log(array.reduce((a, b) => { return a + b;}));
-
+var string = "Hello";
+var string = "Not Hello";
+console.log(string);
