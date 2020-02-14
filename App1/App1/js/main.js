8f712daec618f27cfbd322dd2129a92c8aeedd01
Kenny Strong <kennyspaz@hotmail.com>
2020-02-14 12:23:37 -0700

    All of it 2

---
+++ App1/App1/js/main.js
@@ -0,0 +1,5 @@
+﻿// Your code here!
+let arrays = [[1, 2, 3], [4, 5], [6]];
+let array = [1, 2, 3, 4];
+console.log(array.reduce(a, b) => a + b;);
+//console.log(arrays.reduce(bigArray, tempArray) => bigArray = bigArray.concat(tempArray))
