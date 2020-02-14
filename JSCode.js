37f0fc07d6e1e52be424a8e0737ccd0228df1cbc
Kenny Strong <kennyspaz@hotmail.com>
2020-02-14 13:51:23 -0700

    First commit I think

--- JSCode.js
+++ JSCode.js
@@ -1,5 +1,5 @@
-/*let arrays = [[1, 2, 3], [4, 5], [6]];
+let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrays.reduce((bigArray, tempArray) =>  { return bigArray.concat(tempArray);}));
 let array = [1, 2, 3, 4];
-console.log(array.reduce((a, b) => { return a + b;}));*/
+console.log(array.reduce((a, b) => { return a + b;}));
 
